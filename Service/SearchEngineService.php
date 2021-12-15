191c4b2ddebddd97d7d7a48564274474fe6f8065
Alejandro Glejberman <alejandro@acilia.es>
2021-12-15 15:12:02 -0300

    4.4

--- Service/SearchEngineService.php
+++ Service/SearchEngineService.php
@@ -3,27 +3,20 @@
 namespace Acilia\Bundle\SearchEngineBundle\Service;
 
 use Acilia\Bundle\SearchEngineBundle\Library\SearchEngineException;
-use PDOException;
-use PDOStatement;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class SearchEngineService
 {
-    private $config = [];
+    protected array $config = [];
+    protected ParameterBagInterface $params;
+    private \PDO $connection = null;
 
-    private $connection = null;
-
-    /**
-     * @param string $host
-     * @param string $port
-     * @param string $user
-     * @param string $password
-     */
-    public function __construct($host, $port, $user, $password)
+    public function __construct(ParameterBagInterface $params)
     {
-        $this->config['host'] = $host;
-        $this->config['port'] = $port;
-        $this->config['user'] = $user;
-        $this->config['password'] = $password;
+        $this->config['host'] = $params->get('search.host');
+        $this->config['port'] = $params->get('search.port');
+        $this->config['user'] = $params->get('search.user');
+        $this->config['password'] = $params->get('search.pass');
     }
 
     /**
@@ -31,7 +24,7 @@ class SearchEngineService
      *
      * @throws SearchEngineException
      */
-    protected function connect()
+    protected function connect(): void
     {
         if ($this->config['host'] == 'localhost') {
             $this->config['host'] = '127.0.0.1';
@@ -40,27 +33,28 @@ class SearchEngineService
         try {
             $dsn = 'mysql:host=%host%;port=%port%;charset=utf8;';
             $dsn = str_replace(['%host%', '%port%'], [$this->config['host'], $this->config['port']], $dsn);
+
             $this->connection = new \PDO($dsn, $this->config['user'], $this->config['password']);
-        } catch (PDOException $e) {
+        } catch (\PDOException $e) {
             throw new SearchEngineException(sprintf('Could not connect to Sphinx daemon (%s)', $e->getMessage()));
         }
     }
 
-    public function query($query)
+    public function query(string $query): \PDOStatement
     {
         if ($this->connection ==  null) {
             $this->connect();
         }
 
         $result = $this->connection->query($query);
-        if (!$result instanceof PDOStatement) {
+        if (!$result instanceof \PDOStatement) {
             throw new SearchEngineException('Incorrect results retrieved from Sphinx daemon');
         }
 
         return $result;
     }
 
-    public function escapeQuery($query)
+    public function escapeQuery(string $query): string
     {
         $from = array('\\', '(',')','|','-','!','@','~','"','&', '/', '^', '$', '=', "'", "\x00", "\n", "\r", "\x1a");
         $to = array('\\\\', '\\\(','\\\)','\\\|','\\\-','\\\!','\\\@','\\\~','\\\"', '\\\&', '\\\/', '\\\^', '\\\$', '\\\=', "\\'", '\\x00', '\\n', '\\r', '\\x1a');
